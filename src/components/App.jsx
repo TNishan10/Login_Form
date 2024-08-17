5fbb7fa348f42c5106b2d2ec1adf2b24996b5485
NISHAN THAPA <138146815+TNishan10@users.noreply.github.com>
2024-08-17 15:06:45 +0545

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Form";
+
+var isLoggedIn = false;
+
+function App() {
+  return (
+    <div className="container">
+      {isLoggedIn === true ? <h1>Hello</h1> : <Form />}
+    </div>
+  );
+}
+
+export default App;
